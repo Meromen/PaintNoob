564992267b8f55f9a6fcede138ea54529c96b293
Meromen <git@github.com:Meromen/PaintNoob.git>
2017-12-14 22:49:15 +1000

    add new modul with constants

---
+++ unitconstants.pas
@@ -0,0 +1,24 @@
+unit UnitConstants;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+
+const
+     LineWidthLabel = 'Pen Width';
+     PenStyleLabel  = 'Pen Style';
+     PenColorLabel = 'Pen Color';
+     FillColorLabel = 'Fill Color';
+     FillStyleLabel = 'Fill Style';
+     Radius = 'Radius';
+
+implementation
+
+
+
+end.
+
